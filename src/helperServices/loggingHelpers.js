5518331ba20979ded74dd8b79a200ed818360a1c
Adam Stratilík <adam.stratilik@gstudent.ssnd.sk>
2023-12-03 07:38:06 +0100

    Add logging functionality to track enrolled courses

--- src/helperServices/loggingHelpers.js
+++ src/helperServices/loggingHelpers.js
@@ -1,4 +1,5 @@
 import chalk from 'chalk'
+import fs from 'fs'
 
 export async function logEnrollmentStatus(url, pageTitle) {
   if (!url) {
@@ -6,8 +7,21 @@ export async function logEnrollmentStatus(url, pageTitle) {
     return;
   }
   if (url.startsWith("https://www.udemy.com/cart/success/")) {
-    console.log(chalk.white.bgGreen("+ Enrolled:", pageTitle), "\nURL:", url);
+    console.log(chalk.white.bgGreen("+ Enrolled:", pageTitle));
+    const log = `${pageTitle}\n`;
+    writeLog('logs/enrolledCourses.log', log);
   } else {
     console.log(chalk.white.bgRed("- Enrollment failed:", pageTitle), "\nURL:", url);
+  }
+}
+
+function writeLog(logFile, log) {
+  if (!fs.existsSync(logFile)) {
+    fs.writeFileSync(logFile, log);
+  } else {
+    const logExists = fs.readFileSync(logFile, 'utf8').includes(log);
+    if (!logExists) {
+      fs.appendFileSync(logFile, log);
+    }
   }
 }
