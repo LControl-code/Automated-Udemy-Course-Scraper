7088c6c7ad43cc7278b2ea1ada65b83cf44cc8cb
Adam Stratilík <adam.stratilik@gstudent.ssnd.sk>
2023-12-04 18:57:16 +0100

    Fix scrapeSite error handling

--- src/main/app.js
+++ src/main/app.js
@@ -4,14 +4,23 @@ import scrapeSite from './scrapeSite.js';
 
 // This function checks if we should scrape the site, and if so, it does.
 // After it's done, it schedules itself to run again in 60 seconds.
+let failCount = 0;
 async function runAndReschedule() {
   // Check if we should scrape the site
-  if (await shouldScrape()) {
-    console.log(`+ Scraping enabled`);
-    // If we should, then scrape the site
-    await scrapeSite();
-  } else {
-    console.log(`- Scraping disabled`);
+  try {
+    if (await shouldScrape()) {
+      console.log(`+ Scraping enabled`);
+      await scrapeSite();
+      failCount = 0;
+    } else {
+      console.log(`- Scraping disabled`);
+    }
+  } catch (error) {
+    failCount++;
+    console.error(`ScrapeSite failed ${failCount} time(s).`);
+    if (failCount >= 5) {
+      throw new Error('ScrapeSite failed 5 times. Stopping execution.');
+    }
   }
 
   // Schedule this function to run again in 60 seconds
