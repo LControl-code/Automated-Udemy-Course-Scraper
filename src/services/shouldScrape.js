152595d383b687a77b4e7731660f2a23b30405ae
Adam Stratilík <adam.stratilik@gstudent.ssnd.sk>
2023-12-29 16:31:57 +0100

    Refactor shouldScrape.js to use fs/promises and add eTag checking

--- src/services/shouldScrape.js
+++ src/services/shouldScrape.js
@@ -1,26 +1,63 @@
 import axios from 'axios';
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import { writeLog } from '../helperServices/loggingHelpers.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default async function shouldScrape() {
   const url = process.env.BACKEND_URL_POLLING;
-  const eTagFilePath = path.resolve('data', 'eTag.json')
+  const eTagFilePath = path.resolve('data', 'eTag.json');
+  const eTag = await getETag(eTagFilePath);
 
-  if (!fs.existsSync(eTagFilePath)) {
+  const newETag = await checkForUpdates(url, eTag);
+  if (newETag === null) {
+    return false;
+  }
+
+  await writeLog(eTagFilePath, JSON.stringify({ eTag: newETag }), 'r');
+  return true
+}
+
+async function getETag(eTagFilePath) {
+  try {
+    await fs.access(eTagFilePath);
+  } catch (error) {
     await writeLog(eTagFilePath, JSON.stringify({ eTag: '' }), 'r');
+    console.error('Error: eTag.json file not found. Creating new file.');
   }
 
   let eTagData = { eTag: '' };
-  const fileContent = fs.readFileSync(eTagFilePath, 'utf8');
+  const fileContent = await fs.readFile(eTagFilePath, 'utf8');
   if (fileContent) {
     eTagData = JSON.parse(fileContent);
   }
 
-  const response = await axios.head(url);
-  const currentEtag = response.headers['etag'];
-  if (currentEtag !== eTagData.eTag) {
-    await writeLog(eTagFilePath, JSON.stringify({ eTag: currentEtag }), 'r');
+  return eTagData.eTag;
+}
+
+async function checkForUpdates(url, oldETag) {
+  try {
+    const response = await axios.head(url, {
+      headers: {
+        'If-None-Match': oldETag
+      },
+      validateStatus: function (status) {
+        return (status >= 200 && status < 300) || status === 304;
+      },
+    });
+
+    if (response.status === 304) {
+      console.log('No updates');
+    } else if (response.status === 200) {
+      //// console.log('Updates available');
+      // Update your stored ETag
+      const newETag = response.headers.etag;
+      //// console.log('New ETag:', newETag);
+      return newETag;
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
   }
-  return currentEtag !== eTagData.eTag;
+  return null;
 }
